db6321b2a1a0524e9084ecb8c2c245eff9cd1b29
SylviaHe0716 <132827005+SylviaHe0716@users.noreply.github.com>
2023-12-10 03:05:29 +0800

    Add files via upload

---
+++ Project 2/part3/topo_112109045.py
@@ -0,0 +1,26 @@
+from mininet.topo import Topo
+
+class Project2_Topo_112109045( Topo ):
+    def __init__( self ):
+        Topo.__init__( self )
+
+        # Add hosts
+        h1 = self.addHost( 'h1' )
+        h2 = self.addHost( 'h2' )
+        h3 = self.addHost( 'h3' )
+
+        # Add switches
+        s1 = self.addSwitch( 's1' )
+        s2 = self.addSwitch( 's2' )
+        s3 = self.addSwitch( 's3' )
+        
+        # Add links
+        self.addLink( h1, s1 )
+        self.addLink( h2, s2 )
+        self.addLink( h3, s3 )
+        self.addLink( s1, s2 )
+        self.addLink( s2, s3 )
+        self.addLink( s3, s1 )
+
+
+topos = { 'topo_112109045': Project2_Topo_112109045 }
